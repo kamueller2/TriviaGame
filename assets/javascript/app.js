b84b545d2ee480b72aa9115c3f32014d846e05ee
Kimberly Mueller <kamueller2@gmail.com>
2019-06-06 20:25:53 -0400

    added question object and additional methods

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,49 @@
 //VARIABLES
 
+let questions = [{
+        question: "Who is the oldest of the Kardashian sisters?",
+        answers: ["Kourtney", "Kim", "Khloe", "Kendell"],
+        correct: "Kourtney",
+    }, {
+        question: "Who wrote the McDonald’s jingle, “I’m Lovin’ It?”",
+        answers: ["Justin Timberlake", "Pusha T", "Paul McCartney", "Max Martin"],
+        correct: "Pusha T",
+    }, {
+        question: "What was the original name of Destiny's Child?",
+        answers: ["Girls Tyme", "Blaque", "Patron Saints", "702"],
+        correct: "Girls Tyme",
+    }
+
+
+
+];
+
+let game = {
+    questions: questions,
+    currentQuestion: 0,
+    counter: 30,
+    correct: 0,
+    wrong: 0,
+    timer: function() {
+
+    },
+    loadQuestion: function() {
+
+    },
+    nextQuestion: function() {
+
+    },
+    timeUp: function() {
+
+    },
+    results: function() {
+
+    },
+    clicked: function() {
+
+    }
+}
+
 
 //FUNCTIONS
 
